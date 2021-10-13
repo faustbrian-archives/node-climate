bca1f0bbc6d8ddc88c91873414b7e98b05c73839
Brian Faust <hello@basecode.sh>
2021-10-13 16:45:50 +0300

    wip

--- source/index.ts
+++ source/index.ts
@@ -1,30 +1,31 @@
 import { Command as Clipanion } from "clipanion";
+import { PackageJson } from "type-fest";
 
-import { AppHeader } from "./app-header";
-import { Ask } from "./ask";
-import { AskDate } from "./ask-date";
-import { AskHidden } from "./ask-hidden";
-import { AskNumber } from "./ask-number";
-import { AskPassword } from "./ask-password";
-import { AutoComplete } from "./auto-complete";
-import { Box } from "./box";
-import { Clear } from "./clear";
+import { AppHeader } from "./components/app-header";
+import { Ask } from "./components/ask";
+import { AskDate } from "./components/ask-date";
+import { AskHidden } from "./components/ask-hidden";
+import { AskNumber } from "./components/ask-number";
+import { AskPassword } from "./components/ask-password";
+import { AutoComplete } from "./components/auto-complete";
+import { Box } from "./components/box";
+import { Clear } from "./components/clear";
 import { ILogger } from "./components/contracts";
-import { Error } from "./error";
-import { Fatal } from "./fatal";
-import { Info } from "./info";
-import { Listing } from "./listing";
-import { Log } from "./log";
-import { MultiSelect } from "./multi-select";
-import { NewLine } from "./new-line";
-import { Select } from "./select";
-import { Spinner } from "./spinner";
-import { Success } from "./success";
-import { Table } from "./table";
-import { TaskList } from "./task-list";
-import { Title } from "./title";
-import { Toggle } from "./toggle";
-import { Warning } from "./warning";
+import { Error } from "./components/error";
+import { Fatal } from "./components/fatal";
+import { Info } from "./components/info";
+import { Listing } from "./components/listing";
+import { Log } from "./components/log";
+import { MultiSelect } from "./components/multi-select";
+import { NewLine } from "./components/new-line";
+import { Select } from "./components/select";
+import { Spinner } from "./components/spinner";
+import { Success } from "./components/success";
+import { Table } from "./components/table";
+import { TaskList } from "./components/task-list";
+import { Title } from "./components/title";
+import { Toggle } from "./components/toggle";
+import { Warning } from "./components/warning";
 
 export class Command extends Clipanion {
   async execute(): Promise<void> {
@@ -32,103 +33,103 @@ export class Command extends Clipanion {
   }
 
   protected appHeader(): AppHeader {
-    return new AppHeader(this.logger());
+    return new AppHeader(this.logger(), this.packageJson());
   }
 
   protected ask(): Ask {
-    return new Ask(this.logger());
+    return new Ask(this.logger(), this.packageJson());
   }
 
   protected askDate(): AskDate {
-    return new AskDate(this.logger());
+    return new AskDate(this.logger(), this.packageJson());
   }
 
   protected askHidden(): AskHidden {
-    return new AskHidden(this.logger());
+    return new AskHidden(this.logger(), this.packageJson());
   }
 
   protected askNumber(): AskNumber {
-    return new AskNumber(this.logger());
+    return new AskNumber(this.logger(), this.packageJson());
   }
 
   protected askPassword(): AskPassword {
-    return new AskPassword(this.logger());
+    return new AskPassword(this.logger(), this.packageJson());
   }
 
   protected autoComplete(): AutoComplete {
-    return new AutoComplete(this.logger());
+    return new AutoComplete(this.logger(), this.packageJson());
   }
 
   protected box(): Box {
-    return new Box(this.logger());
+    return new Box(this.logger(), this.packageJson());
   }
 
   protected clear(): Clear {
-    return new Clear(this.logger());
+    return new Clear(this.logger(), this.packageJson());
   }
 
   protected error(): Error {
-    return new Error(this.logger());
+    return new Error(this.logger(), this.packageJson());
   }
 
   protected fatal(): Fatal {
-    return new Fatal(this.logger());
+    return new Fatal(this.logger(), this.packageJson());
   }
 
   protected info(): Info {
-    return new Info(this.logger());
+    return new Info(this.logger(), this.packageJson());
   }
 
   protected listing(): Listing {
-    return new Listing(this.logger());
+    return new Listing(this.logger(), this.packageJson());
   }
 
   protected log(): Log {
-    return new Log(this.logger());
+    return new Log(this.logger(), this.packageJson());
   }
 
   protected multiSelect(): MultiSelect {
-    return new MultiSelect(this.logger());
+    return new MultiSelect(this.logger(), this.packageJson());
   }
 
   protected newLine(): NewLine {
-    return new NewLine(this.logger());
+    return new NewLine(this.logger(), this.packageJson());
   }
 
   protected prompt(): Prompt {
-    return new Prompt(this.logger());
+    return new Prompt(this.logger(), this.packageJson());
   }
 
   protected select(): Select {
-    return new Select(this.logger());
+    return new Select(this.logger(), this.packageJson());
   }
 
   protected spinner(): Spinner {
-    return new Spinner(this.logger());
+    return new Spinner(this.logger(), this.packageJson());
   }
 
   protected success(): Success {
-    return new Success(this.logger());
+    return new Success(this.logger(), this.packageJson());
   }
 
   protected table(): Table {
-    return new Table(this.logger());
+    return new Table(this.logger(), this.packageJson());
   }
 
   protected taskList(): TaskList {
-    return new TaskList(this.logger());
+    return new TaskList(this.logger(), this.packageJson());
   }
 
   protected title(): Title {
-    return new Title(this.logger());
+    return new Title(this.logger(), this.packageJson());
   }
 
   protected toggle(): Toggle {
-    return new Toggle(this.logger());
+    return new Toggle(this.logger(), this.packageJson());
   }
 
   protected warning(): Warning {
-    return new Warning(this.logger());
+    return new Warning(this.logger(), this.packageJson());
   }
 
   protected logger(): ILogger {
@@ -137,5 +138,9 @@ export class Command extends Clipanion {
       info: console.info,
       warning: console.warn,
     };
+  }
+
+  protected packageJson(): PackageJson {
+    return require("../package.json");
   }
 }
